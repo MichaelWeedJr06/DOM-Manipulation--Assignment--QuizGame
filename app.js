21991641214b43dc43c492e9330c84beee2219b5
MiWeed <michaelweedjr@gmail.com>
2024-10-11 13:57:08 -0400

    almost done

--- app.js
+++ app.js
@@ -1,43 +1,3 @@
-let answers = document.querySelectorAll(".answer");
-let QuestionList = [];
-let answeredCorrect = document.getElementsByClassName("num");
-const next = document.getElementById("next");
-let Count = document.getElementById("corr");
-After = document.querySelector(".after");
-let text = document.getElementsByClassName("Text");
-let nextButton = document.getElementById("next");
-nextButton.addEventListener("click", Next);
-let correctCount = 0;
-let correctAnswerIndex = 0;
-let Index = 0;
-let image = document.getElementsByTagName("img")[0];
-let question = document.getElementsByClassName("question")[0];
-function OnClick() {
-  if (this.innerText == QuestionList[Index].answs[correctAnswerIndex]) {
-    After.classList.toggle("win");
-    text.innerText = "Correct";
-    correctCount += 1;
-    correctCount += parseInt(answeredCorrect.innerHTML);
-    answeredCorrect.innerText = toString(correctCount);
-    index = QuestionList.indexOf(Index);
-    QuestionList.pop(index);
-  } else {
-    this.backgroundColor = "red";
-    After.classList.toggle("win");
-    text.innerText = "Wrong";
-    index = QuestionList.indexOf(Index);
-    QuestionList.pop(index);
-  }
-  for (let i = 0; i < answers.length; i++) {
-    answers[i].disabled = true;
-  }
-}
-for (let i = 0; i < answers.length; i++) {
-  answers[i].addEventListener("click", OnClick);
-}
-function Next() {
-  SetScreen();
-}
 const questions = [
   {
     question: "Who is this guy?",
@@ -80,23 +40,88 @@ const questions = [
     answs: ["Logan", "Aiden", "Preston", "Michael"],
   },
 ];
-for (let i = 0; i < questions.length; i++) {
-  QuestionList.push(...questions);
+let QuestionList = [];
+QuestionList.push(...questions);
+
+let answers = document.querySelectorAll(".answer");
+let answeredCorrect = document.getElementById("num");
+const next = document.getElementById("next");
+let Count = document.getElementById("corr");
+After = document.querySelector(".after");
+let text = document.getElementsByClassName("Text")[0];
+let nextButton = document.getElementById("next");
+nextButton.addEventListener("click", Next);
+let correctCount = 0;
+let correctAnswerIndex;
+let Index = 0;
+let image = document.getElementsByTagName("img")[0];
+let question = document.getElementsByClassName("question")[0];
+for (let i = 0; i < answers.length; i++) {
+  answers[i].addEventListener("click", OnClick);
+  answers[i].classList.add("answerss");
+}
+function OnClick() {
+  After.style.display = "block";
+  //After.classList.toggle("win");
+  if (QuestionList.length == 0) {
+    for (let i = 0; i < answers.length; i++) {
+      answers[i].style.display = "none";
+      question.innerText = "Quiz Done!!!!";
+    }
+  }
+  if (this.innerText === QuestionList[Index].answs[correctAnswerIndex]) {
+    text.innerText = "Correct";
+    this.classList.add("correct");
+    correctCount += 1;
+    answeredCorrect.innerText = correctCount;
+
+    QuestionList.splice(Index, 1);
+  } else {
+    this.classList.add("wrong");
+    for (let i = 0; i < answers.length; i++) {
+      if (
+        answers[i].innerText == QuestionList[Index].answs[correctAnswerIndex]
+      ) {
+        QuestionList.splice(Index, 1);
+        answers[i].classList.add("correct");
+      } else {
+        text.innerText = "Wrong";
+        QuestionList.splice(Index, 1);
+      }
+    }
+  }
+  for (let i = 0; i < answers.length; i++) {
+    answers[i].classList.remove("answerss");
+    answers[i].disabled = true;
+  }
+}
+function CleanButtons() {
+  for (let i = 0; i < answers.length; i++) {
+    answers[i].classList.remove("correct");
+    answers[i].classList.remove("wrong");
+  }
+}
+
+function Next() {
+  SetScreen();
 }
 
 function SetScreen() {
-  After.classList.toggle("after");
+  After.style.display = "none";
+
+  //After.classList.toggle("after");
   for (let i = 0; i < answers.length; i++) {
     answers[i].disabled = false;
   }
+  CleanButtons();
   question = Math.floor(Math.random() * QuestionList.length);
   Index = question;
-  correctAnswerIndex = QuestionList[question].correctAnswer;
-  question.innerText = QuestionList[question].question;
-  image.src = QuestionList[question].img;
+  correctAnswerIndex = QuestionList[Index].correctAnswer;
+  question.innerText = QuestionList[Index].question;
+  image.src = QuestionList[Index].img;
   for (let i = 0; i < QuestionList[question].answs.length; i++) {
     answers[i].innerText = QuestionList[question].answs[i];
   }
 }
-
+answeredCorrect.innerText = correctCount;
 SetScreen();
